590aa29a30d0b13618fdd3726a794de41bec581d
Bonang2005 <mahlakobonang@gmail.com>
2026-04-10 02:53:14 +0200

    program class: Added user input and string manipulation

--- CyberSecurityChatBot/Program.cs
+++ CyberSecurityChatBot/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CyberSecurityChatBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Speech.Synthesis;
+
+AudioPlayer.playgreeting();
+Display.ShowLogo();
+WriteLine("Enter your name:");
+string name = ReadLine();
+
+while (string.IsNullOrEmpty(name))
+{
+    ForegroundColor = ConsoleColor.Red;
+    WriteLine("please enter your name.");
+    ResetColor();
+    Write("Enter your name:");
+    name = ReadLine();
+}
