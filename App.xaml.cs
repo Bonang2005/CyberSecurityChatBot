1a3d55e48bc9c1f542e6402a3c952776fad61a3f
Bonang2005 <mahlakobonang@gmail.com>
2026-06-24 20:57:13 +0200

    Initial commit: Set up Part 3 WPF project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CybersecurityChatbot_Part3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
